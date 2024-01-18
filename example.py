c650e2de0415cea33a3e81f7d046222326bb100b
2sarajade <2sarajade@gmail.com>
2024-01-17 20:21:10 -0800

    tests complete

--- example.py
+++ example.py
@@ -10,20 +10,28 @@ def main():
     """
     # Create instance of FastaParser
     # Create instance of FastqParser
+    fasta_p = FastaParser("data/test.fa")
+    fastq_p = FastqParser("data/test.fq")
         
     # For each record of FastaParser, Transcribe the sequence
     # and print it to console
+    for record in fasta_p:
+        print(transcribe(record[1]))
        
     # For each record of FastqParser, Transcribe the sequence
     # and print it to console
-
+    for record in fastq_p:
+        print(transcribe(record[1]))
 
     # For each record of FastaParser, Reverse Transcribe the sequence
     # and print it to console
+    for record in fasta_p:
+        print(reverse_transcribe(record[1]))
        
     # For each record of FastqParser, Reverse Transcribe the sequence
     # and print it to console
-
+    for record in fastq_p:
+        print(reverse_transcribe(record[1]))
 
 """
 When executing a python script from the command line there will
--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -33,7 +33,18 @@ def test_FastaParser():
     files that are blank or corrupted in some way. Two example Fasta files are
     provided in /tests/bad.fa and /tests/empty.fa
     """
-    pass
+    fasta = FastaParser("data/test.fa")
+    seqs = list(fasta)
+    assert len(seqs) == 100
+    assert seqs[0] == ("seq0", "TGATTGAATCTTTTGAGGGTCACGGCCCGGAAGCCAGAATTTCGGGGTCCTCTGTGGATATTAATCGAGCCCACACGGTGTGAGTTCAGCGGCCCCCGCA")
+    assert seqs[99] == ("seq99", "CAAACCGGCGATGCGGGTACTCCCTACAAGTTGGACTCCGCAGCGAACGCCGCAGGGGCCATTATACGGCGGTCTTGGCGGCGTCGACCAGGCCGGTCCA")
+    
+    fasta = FastaParser("tests/bad.fa")
+    with pytest.raises(ValueError):
+        list(fasta)
+    fasta = FastaParser("tests/blank.fa")
+    with pytest.raises(ValueError):
+        list(fasta)
 
 
 def test_FastaFormat():
@@ -41,7 +52,19 @@ def test_FastaFormat():
     Test to make sure that a fasta file is being read in if a fastq file is
     read, the first item is None
     """
-    pass
+    fasta_real = FastaParser("data/test.fa")
+    fasta_wrong = FastaParser("data/test.fq")
+
+    unwanted_none = False
+    for seq in fasta_real:
+        if seq[0] is None or seq[1] is None:
+            unwanted_none = True
+    found_none = False
+    for seq in fasta_wrong:
+        if seq[0] is None or seq[1] is None:
+            found_none = True
+    assert not unwanted_none
+    assert found_none
 
 
 def test_FastqParser():
@@ -50,11 +73,34 @@ def test_FastqParser():
     an instance of your FastqParser class and assert that it properly reads 
     in the example Fastq File.
     """
-    pass
+    fastq = FastqParser("data/test.fq")
+    seqs = list(fastq)
+    assert len(seqs) == 100
+    assert seqs[0] == ("seq0", "TGTGGTCGTATAGTTATTGTCATAAATTACACAGAATCGCGATTCTCCGCGTCCACCAATCTTAGTGCACCACAGCATCGACCCGATTTATGACGCTGAG", "*540($=*,=.062565,2>'487')!:&&6=,6,*7>:&132&83*8(58&59>'8!;28<94,0*;*.94**:9+7\"94(>7='(!5\"2/!%\"4#32=")
+    assert seqs[99] == ("seq99", "CCGAGTTTTGTAGTGGGCTCAACTGAAATCCTATTCTTAGACGATTGGTCATAAAACCCTTTCACTGTACGGACGTAGACCCTGCTCCGTCTTCCAGCAG", "2$7)*5:\"=+++!:.=>!5>79)8!566$!3*/4$=4.%=//;900$9)!%)4%$=0\":02\"0=!0#/>+*1$1$39!.8+9<'1$*1$321&<'&9,)2")
+    
+    fastq = FastqParser("tests/bad.fq")
+    with pytest.raises(ValueError):
+        list(fastq)
+    fastq = FastqParser("tests/blank.fq")
+    with pytest.raises(ValueError):
+        list(fastq)
 
 def test_FastqFormat():
     """
     Test to make sure fastq file is being read in. If this is a fasta file, the
     first line is None
     """
-    pass
+    fastq_real = FastqParser("data/test.fq")
+    fastq_wrong = FastqParser("data/test.fa")
+
+    unwanted_none = False
+    for seq in fastq_real:
+        if seq[0] is None or seq[1] is None or seq[2] is None:
+            unwanted_none = True
+    found_none = False
+    for seq in fastq_wrong:
+        if seq[0] is None or seq[1] is None or seq[2] is None:
+            found_none = True
+    assert not unwanted_none
+    assert found_none
